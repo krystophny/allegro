7e53dd70d2df8cf73fdd4517a496771523778f72
Christopher Albert <albert@alumni.tugraz.at>
2024-09-21 21:33:40 +0900

    Removed deleted libs mac

--- src/macosx/qzfull.m
+++ src/macosx/qzfull.m
@@ -1,6 +1,6 @@
-/*         ______   ___    ___ 
- *        /\  _  \ /\_ \  /\_ \ 
- *        \ \ \L\ \\//\ \ \//\ \      __     __   _ __   ___ 
+/*         ______   ___    ___
+ *        /\  _  \ /\_ \  /\_ \
+ *        \ \ \L\ \\//\ \ \//\ \      __     __   _ __   ___
  *         \ \  __ \ \ \ \  \ \ \   /'__`\ /'_ `\/\`'__\/ __`\
  *          \ \ \/\ \ \_\ \_ \_\ \_/\  __//\ \L\ \ \ \//\ \L\ \
  *           \ \_\ \_\/\____\/\____\ \____\ \____ \ \_\\ \____/
@@ -31,13 +31,9 @@
 static BITMAP *osx_qz_full_init(int, int, int, int, int);
 static void osx_qz_full_exit(BITMAP *);
 static void osx_qz_full_vsync(void);
-static void osx_qz_full_set_palette(AL_CONST struct RGB *, int, int, int);
 static int osx_qz_show_video_bitmap(BITMAP *);
 static GFX_MODE_LIST *osx_qz_fetch_mode_list(void);
 
-
-CGDirectPaletteRef osx_palette = NULL;
-int osx_palette_dirty = FALSE;
 int osx_screen_used;
 
 static int lock_nesting = 0;
@@ -52,14 +48,14 @@ static BITMAP *old_visible_bmp = NULL;
 GFX_DRIVER gfx_quartz_full =
 {
    GFX_QUARTZ_FULLSCREEN,
-   empty_string, 
    empty_string,
-   "Quartz fullscreen", 
+   empty_string,
+   "Quartz fullscreen",
    osx_qz_full_init,
    osx_qz_full_exit,
    NULL,                         /* AL_METHOD(int, scroll, (int x, int y)); */
    osx_qz_full_vsync,
-   osx_qz_full_set_palette,
+   NULL,
    NULL,                         /* AL_METHOD(int, request_scroll, (int x, int y)); */
    NULL,                         /* AL_METHOD(int, poll_scroll, (void)); */
    NULL,                         /* AL_METHOD(void, enable_triple_buffer, (void)); */
@@ -92,7 +88,7 @@ GFX_DRIVER gfx_quartz_full =
 void osx_init_fade_system(void)
 {
    CGTableCount samples;
-   
+
    CGGetDisplayTransferByTable(kCGDirectMainDisplay, 256, &original_table[0], &original_table[256], &original_table[512], &samples);
 }
 
@@ -104,7 +100,7 @@ void osx_fade_screen(int fade_in, double seconds)
    int i, j;
    double factor;
    CGGammaValue table[768];
-   
+
    for (i = 0; i < FADE_STEPS; i++) {
       if (fade_in)
          factor = (double)i / (double)FADE_STEPS;
@@ -133,7 +129,7 @@ static BITMAP *private_osx_qz_full_init(int w, int h, int v_w, int v_h, int colo
    boolean_t match = FALSE;
    int bpp, refresh_rate;
    char tmp1[128];
-   
+
    if (1
 #ifdef ALLEGRO_COLOR8
        && (color_depth != 8)
@@ -153,15 +149,15 @@ static BITMAP *private_osx_qz_full_init(int w, int h, int v_w, int v_h, int colo
       w = 320;
       h = 200;
    }
-   
+
    if (v_w < w) v_w = w;
    if (v_h < h) v_h = h;
-   
+
    if ((v_w != w) || (v_h != h)) {
       ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Resolution not supported"));
       return NULL;
    }
-   
+
    bpp = color_depth == 15 ? 16 : color_depth;
    if (_refresh_rate_request > 0)
       mode = CGDisplayBestModeForParametersAndRefreshRate(kCGDirectMainDisplay, bpp, w, h,
@@ -172,7 +168,7 @@ static BITMAP *private_osx_qz_full_init(int w, int h, int v_w, int v_h, int colo
       ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Resolution not supported"));
       return NULL;
    }
-   
+
    osx_init_fade_system();
    old_mode = CGDisplayCurrentMode(kCGDirectMainDisplay);
    osx_fade_screen(FALSE, 0.2);
@@ -186,13 +182,10 @@ static BITMAP *private_osx_qz_full_init(int w, int h, int v_w, int v_h, int colo
    }
    HideMenuBar();
    CGDisplayRestoreColorSyncSettings();
-   
+
    CFNumberGetValue(CFDictionaryGetValue(mode, kCGDisplayRefreshRate), kCFNumberSInt32Type, &refresh_rate);
    _set_current_refresh_rate(refresh_rate);
-   
-   if (CGDisplayCanSetPalette(kCGDirectMainDisplay))
-      osx_palette = CGPaletteCreateDefaultColorPalette();
-   
+
    bmp = _make_bitmap(w, h, (unsigned long)CGDisplayBaseAddress(kCGDirectMainDisplay),
                       &gfx_quartz_full, color_depth, CGDisplayBytesPerRow(kCGDirectMainDisplay));
    if (bmp)
@@ -201,34 +194,34 @@ static BITMAP *private_osx_qz_full_init(int w, int h, int v_w, int v_h, int colo
       ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Not enough memory"));
       return NULL;
    }
-   
+
    screen_port = CreateNewPortForCGDisplayID((UInt32)kCGDirectMainDisplay);
    if (!screen_port) {
       ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Cannot create DirectDisplay port"));
       return NULL;
    }
    BMP_EXTRA(bmp)->port = screen_port;
-   
+
    setup_direct_shifts();
-   
+
    gfx_quartz_full.w = w;
    gfx_quartz_full.h = h;
    gfx_quartz_full.vid_mem = w * h * BYTES_PER_PIXEL(color_depth);
-   
+
    _screen_vtable.created_sub_bitmap = osx_qz_created_sub_bitmap;
    if (color_depth != 8)
       _screen_vtable.blit_to_self = osx_qz_blit_to_self;
-   
+
    uszprintf(driver_desc, sizeof(driver_desc), uconvert_ascii("CoreGraphics DirectDisplay access, %d bpp", tmp1),
              color_depth);
    gfx_quartz_full.desc = driver_desc;
-   
+
    osx_keyboard_focused(FALSE, 0);
    clear_keybuf();
    osx_gfx_mode = OSX_GFX_FULL;
    osx_skip_mouse_move = TRUE;
    osx_screen_used = FALSE;
-   
+
    if (color_depth != 8) {
       gfx_quartz_full.set_mouse_sprite = osx_mouse_set_sprite;
       gfx_quartz_full.show_mouse = osx_mouse_show;
@@ -243,9 +236,9 @@ static BITMAP *private_osx_qz_full_init(int w, int h, int v_w, int v_h, int colo
       gfx_quartz_full.move_mouse = NULL;
       CGDisplayHideCursor(kCGDirectMainDisplay);
    }
-   
+
    old_visible_bmp = bmp;
-   
+
    return bmp;
 }
 
@@ -268,18 +261,13 @@ static BITMAP *osx_qz_full_init(int w, int h, int v_w, int v_h, int color_depth)
 static void osx_qz_full_exit(BITMAP *bmp)
 {
    _unix_lock_mutex(osx_event_mutex);
-   
+
    if ((bmp) && (bmp->extra)) {
       if (BMP_EXTRA(bmp)->port)
          DisposeGWorld(BMP_EXTRA(bmp)->port);
       free(bmp->extra);
    }
-   
-   if (osx_palette) {
-      CGPaletteRelease(osx_palette);
-      osx_palette = NULL;
-   }
-   
+
    if (old_mode) {
       osx_fade_screen(FALSE, 0.1);
       CGDisplaySwitchToMode(kCGDirectMainDisplay, old_mode);
@@ -291,9 +279,9 @@ static void osx_qz_full_exit(BITMAP *bmp)
       CGDisplayRestoreColorSyncSettings();
       old_mode = NULL;
    }
-   
+
    osx_gfx_mode = OSX_GFX_NONE;
-   
+
    _unix_unlock_mutex(osx_event_mutex);
 }
 
@@ -310,32 +298,6 @@ static void osx_qz_full_vsync(void)
 
 
 
-/* osx_qz_full_set_palette:
- *  Sets palette for quartz fullscreen.
- */
-static void osx_qz_full_set_palette(AL_CONST struct RGB *p, int from, int to, int vsync)
-{
-   int i;
-   CGDeviceColor color;
-   
-   if (!CGDisplayCanSetPalette(kCGDirectMainDisplay))
-      return;
-   
-   _unix_lock_mutex(osx_event_mutex);
-   
-   for (i = from; i <= to; i++) {
-      color.red = ((float)p[i].r / 63.0);
-      color.green = ((float)p[i].g / 63.0);
-      color.blue = ((float)p[i].b / 63.0);
-      CGPaletteSetColorAtIndex(osx_palette, color, i);
-   }
-   osx_palette_dirty = TRUE;
-   
-   _unix_unlock_mutex(osx_event_mutex);
-}
-
-
-
 /* osx_qz_show_video_bitmap:
  *  Displays a video bitmap on the screen by copying it using CopyBits
  *  (hw accelerated copy).
@@ -345,10 +307,10 @@ static int osx_qz_show_video_bitmap(BITMAP *bmp)
    Rect rect;
    unsigned char *addr;
    int i;
-   
+
    if ((bmp->w != gfx_quartz_full.w) || (bmp->h != gfx_quartz_full.h))
       return -1;
-   
+
    SetRect(&rect, 0, 0, bmp->w, bmp->h);
    while (!QDDone(screen_port));
    while (!QDDone(BMP_EXTRA(bmp)->port));
@@ -372,7 +334,7 @@ static int osx_qz_show_video_bitmap(BITMAP *bmp)
       old_visible_bmp->line[i] = addr;
    }
    old_visible_bmp = bmp;
-   
+
    return 0;
 }
 
@@ -390,7 +352,7 @@ static GFX_MODE_LIST *osx_qz_fetch_mode_list(void)
    int i, j, num_modes;
    int width, height, bpp;
    int already_stored;
-   
+
    modes_list = CGDisplayAvailableModes(kCGDirectMainDisplay);
    if (!modes_list)
       return NULL;
@@ -400,14 +362,14 @@ static GFX_MODE_LIST *osx_qz_fetch_mode_list(void)
       return NULL;
    gfx_mode_list->mode = NULL;
    gfx_mode_list->num_modes = 0;
-   
+
    for (i = 0; i < num_modes; i++) {
       mode = CFArrayGetValueAtIndex(modes_list, i);
-      
+
       CFNumberGetValue(CFDictionaryGetValue(mode, kCGDisplayWidth), kCFNumberSInt32Type, &width);
       CFNumberGetValue(CFDictionaryGetValue(mode, kCGDisplayHeight), kCFNumberSInt32Type, &height);
       CFNumberGetValue(CFDictionaryGetValue(mode, kCGDisplayBitsPerPixel), kCFNumberSInt32Type, &bpp);
-      
+
       if (bpp == 16)
          bpp = 15;
       already_stored = FALSE;
@@ -434,7 +396,7 @@ static GFX_MODE_LIST *osx_qz_fetch_mode_list(void)
 	 gfx_mode->bpp = bpp;
       }
    }
-   
+
    if (gfx_mode_list->mode) {
       gfx_mode_list->mode[gfx_mode_list->num_modes].width = 0;
       gfx_mode_list->mode[gfx_mode_list->num_modes].height = 0;
@@ -446,4 +408,3 @@ static GFX_MODE_LIST *osx_qz_fetch_mode_list(void)
    }
    return gfx_mode_list;
 }
-
